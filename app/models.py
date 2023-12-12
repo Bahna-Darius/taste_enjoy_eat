035d03c892268ccf5fb3aaf275a6930c9f1fd5af
Bahna-Darius <emanuelbahna@gmail.com>
2023-12-12 22:43:43 +0200

    Tags create in post.html

--- app/models.py
+++ app/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    # every time we save a Tags in /Tags_Page/ without id
+    # update the slug and return OBJ along with the parameters, and call save()
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        return super(Tag, self).save(*args, **kwargs)
+
+    # to return to the Admin Panel, change from Object Class to string
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -7,3 +25,5 @@ class Post(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=200, unique=True)
     image = models.ImageField(null=True, blank=True, upload_to="images/")
+    tags = models.ManyToManyField(Tag, blank=True, related_name='post')
+
