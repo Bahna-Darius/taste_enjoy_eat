14120a5cd7803f3ccdf74b4d83b39aa122afa395
Bahna-Darius <emanuelbahna@gmail.com>
2023-12-12 20:19:22 +0200

    Home Page create

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-       path('post/<slug:slug>', views.post_page, name='post_page')
+       path('post/<slug:slug>', views.post_page, name='post_page'),
+       path('', views.index, name='home_page')
 ]
 
--- app/views.py
+++ app/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from app.models import Post
 
 
+def index(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+
+    return render(request, 'app/index.html', context)
+
+
 def post_page(request, slug):
-    post = Post.objects.get(slug=slug)
+    posts = Post.objects.get(slug=slug)
     context = {
-        'post': post
+        'post': posts
     }
 
     return render(request, 'app/post.html', context)
